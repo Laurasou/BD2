a4de6829a2c5b8217de1e983ccac8be515fb1af0
Laurasou <lauracristinasouza45@gmail.com>
2025-07-28 14:27:07 -0300

    Aula 19 finalizada

---
+++ Aula 19/comandosUltizados.sql
@@ -0,0 +1,16 @@
+use loja;
+SELECT * from pedido;
+where cliente_nome like "Carlos%";
+
+SELECT pedido.id, pedido.cliente_nome, pedido.data_pedido,
+ pedido_item.id_estoque, pedido_item.quantidade
+ FROM pedido JOIN pedido_item
+ ON pedido.id = pedido_item.id_pedido 
+ where cliente_nome like "Carlos%";
+ CREATE VIEW Vendas AS
+ SELECT pedido.id, pedido.cliente_nome, pedido.data_pedido,
+ pedido_item.id_estoque, pedido_item.quantidade
+ FROM pedido JOIN pedido_item
+ ON pedido.id = pedido_item.id_pedido;
+ 
+ SELECT * FROM Vendas;
